66a6a12da303c9c0e36c560e92824785f5ad7590
Amir <soheyl637@gmai.com>
2024-07-27 05:39:42 +0100

    refactor(background.js): Simplify browserAction click listener, remove runtime message listener

--- src/background.js
+++ src/background.js
@@ -1,17 +1,7 @@
 const browserAPI = typeof chrome !== 'undefined' ? chrome : browser;
 
 if (browserAPI && browserAPI.browserAction && browserAPI.browserAction.onClicked) {
-  browserAPI.browserAction.onClicked.addListener((tab) => {
-    if (browserAPI.tabs && browserAPI.tabs.sendMessage) {
-      browserAPI.tabs.sendMessage(tab.id, { action: "toggleFont" });
-    }
+  browserAPI.browserAction.onClicked.addListener(() => {
+    browserAPI.browserAction.openPopup();
   });
 }
-
-browserAPI.runtime.onMessage.addListener((message) => {
-  if (message.action === "toggleFont" || message.action === "toggleOverlay") {
-    browserAPI.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      browserAPI.tabs.sendMessage(tabs[0].id, message);
-    });
-  }
-});
